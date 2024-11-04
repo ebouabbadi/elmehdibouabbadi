bb6d28c5b84606f33baa35f0835135f35d85da68
ebouabbadi <ebouabbadi@gmail.com>
2024-11-04 19:12:07 +0100

    add contact and education

--- components/about.tsx
+++ components/about.tsx
@@ -10,40 +10,41 @@ export default function About() {
 
   return (
     <motion.section
-      ref={ref}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
-      id="about"
-    >
-      <SectionHeading>About me</SectionHeading>
-      <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
-      </p>
-
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
-      </p>
-    </motion.section>
+    ref={ref}
+    className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+    initial={{ opacity: 0, y: 100 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ delay: 0.175 }}
+    id="about"
+  >
+    <SectionHeading>About me</SectionHeading>
+    <p className="mb-3">
+      Hello, I'm <span className="font-medium">El Mehdi Bouabbadi</span>, a dedicated{" "}
+      <span className="font-medium">Software Engineer</span> specializing in{" "}
+      <span className="font-medium">Full Stack Development</span> for both{" "}
+      <span className="font-medium">web and mobile applications</span>. With over
+      three years of experience, I've honed my skills in key technologies such as{" "}
+      <span className="font-medium">
+        React.js, React Native, Next.js, Node.js, Nest.js, and Docker
+      </span>
+      . My work spans a wide range of projects, from mobile apps to scalable web
+      solutions, all aimed at enhancing user experiences and meeting business
+      needs.
+    </p>
+  
+    <p className="mb-3">
+      As a problem solver and tech enthusiast, I enjoy diving deep into complex
+      challenges, whether it's refining front-end interfaces for seamless
+      interaction or building secure, efficient back-end systems. I'm passionate
+      about staying ahead of the curve with the latest industry trends and
+      delivering innovative solutions that make a real impact.
+    </p>
+  
+    <p>
+      I'm always excited to take on new challenges and collaborate on projects
+      that push the boundaries of what's possible in software development. Let’s
+      work together to create something exceptional!
+    </p>
+  </motion.section>
   );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -58,16 +58,21 @@ export default function Intro() {
       </div>
 
       <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
-        <span className="font-bold">Hello, I'm El Mehdi Bouabbadi.</span> I'm a{" "}
-        <span className="font-bold">Software Engineer</span> with{" "}
-        <span className="font-bold">2 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
-      </motion.h1>
+  className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+  initial={{ opacity: 0, y: 100 }}
+  animate={{ opacity: 1, y: 0 }}
+>
+  <span className="font-bold">Hello, I'm El Mehdi Bouabbadi.</span> I'm a{" "}
+  <span className="font-bold">Software Engineer</span> specializing in{" "}
+  <span className="font-bold">Mobile and Web Development</span> with expertise in{" "}
+  <span className="font-bold">React, React Native, Next.js, Nest.js,</span> and{" "}
+  <span className="font-bold">Docker</span>. I thrive on solving complex problems.
+</motion.h1>
+
+
+
+
+
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"

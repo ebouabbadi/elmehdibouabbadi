c3eb951be2526e112a2c36592aef95bf7976b14f
EL Mehdi Bouabbadi <84751335+ebouabbadi@users.noreply.github.com>
2024-11-19 18:10:51 +0100

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -116,6 +116,15 @@ export const educationsData = [
 ] as const;
 
 export const projectsData = [
+   {
+    title: "Yawmiyat",
+    description: `Yawmiyat is a modern and user-friendly app designed to help you organize your day, jot down your activities, and track your daily goals. Whether you want to plan your tasks, write down ideas, or recall important moments, Yawmiyat is your personal companion to structure your life and stay productive.`,
+    tags: ["JavaScript", "React.js","node.js","Prisma", "React Native", "Tailwind", "iOS Development", "Android Development", "HTML", "CSS", "Teamwork", "Postman"],
+    imageUrl: "",
+    link: "",
+    isPrivate: true,
+    time: '202419',
+  },
   {
     title: "Startup",
     description: ` Spearheading mobile development projects using React Native, ensuring smooth deployment and performance across iOS and Android platforms.

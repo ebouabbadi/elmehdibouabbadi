5cd2ca281b3a25a63e6dbee9c167627d02404d24
EL Mehdi Bouabbadi <84751335+ebouabbadi@users.noreply.github.com>
2025-03-04 15:32:21 +0000

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -127,7 +127,7 @@ export default function Intro() {
         </a>
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.instagram.com/ebouabbadi"
+          href="https://www.instagram.com/mehdicode.s"
           target="_blank"
         >
           <FaInstagram />
